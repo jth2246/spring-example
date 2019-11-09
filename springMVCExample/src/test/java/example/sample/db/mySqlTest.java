d11aa288ca4da8f9666c998a643dd90ba795d8c8
jth2246 <jth2246@gmail.com>
2019-11-09 17:14:44 +0900

    update DB Test Connection Info for local
    add connection info for local 
    change db connection driver name

--- springMVCExample/src/test/java/example/sample/db/mySqlTest.java
+++ springMVCExample/src/test/java/example/sample/db/mySqlTest.java
@@ -6,11 +6,20 @@ import java.sql.DriverManager;
 import org.junit.Test;
 
 public class mySqlTest {
+
+	/*  DB Connection Info for AWS RDS
+private static final String DRIVER ="com.mysql.cj.jdbc.Driver";
+private static final String URL ="jdbc:mysql://mysql-database.czndabjnrcgf.ap-northeast-2.rds.amazonaws.com"
+								+ ":3306/testmysql";
+private static final String ID ="admin_mysql";
+private static final String PW ="admin_mysql";
+*/
+	// DB Connection Info for local Tomcat
 	private static final String DRIVER ="com.mysql.cj.jdbc.Driver";
-	private static final String URL ="jdbc:mysql://mysql-database.czndabjnrcgf.ap-northeast-2.rds.amazonaws.com"
-									+ ":3306/testmysql";
-	private static final String ID ="admin_mysql";
-	private static final String PW ="admin_mysql";
+	private static final String URL ="jdbc:mysql://127.0.0.1:3306/testmysql?serverTimezone=UTC";
+	private static final String ID ="jth2246";
+	private static final String PW ="dream134679";
+
 	@Test
 	public void testConnection() throws Exception{
 		Class.forName(DRIVER);
